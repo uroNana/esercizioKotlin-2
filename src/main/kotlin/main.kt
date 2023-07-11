0df54857bed37c6ad5cdaca279dc44a784eb9459
uroNana <mirianamartinelli97@libero.it>
2023-07-11 15:54:22 +0200

    exercise performed_4

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,42 +2,56 @@
 // one method - one using inheritance and another using interfaces.
 
 fun main() {
-    val parent = Parent("Salvatore", 58)
-    val children1 = Children1("Alessandro", 27)
-    val children2 = Children2("Mriana", 26)
+    val parent = InheritanceParent("Salvatore", 58)
+    val children1 = InheritanceChildren1("Alessandro", 27)
+    val children2 = InheritanceChildren2("Mriana", 26)
+    val parent1 = Parent("Salvatore", 58)
+    val children3 = Children1("Alessandro", 27)
+    val children4 = Children2("Mriana", 26)
 
-    parent.inheritance()
-    println(parent.interfaces())
+    parent.getDetail()
+    println(parent1.getDetail())
 
-    children1.inheritance()
-    println(children1.interfaces())
+    children1.getDetail()
+    println(children3.getDetail())
 
-    children2.inheritance()
-    println(children2.interfaces())
+    children2.getDetail()
+    println(children4.getDetail())
 
 }
 
 
-class Parent(override val name : String, override val age : Int) : Interfaces, Inheritance(name, age) {
+class Parent(override val name : String, override val age : Int) : PersonInterfaces {
+    override fun getDetail(): String {
+        return "the person's name is $name"
+    }
 }
 
-class Children1(override val name : String, override val age : Int) : Interfaces, Inheritance(name, age) {
+class InheritanceParent(name : String, age : Int) : PersonInheritance(name, age) {
+}
+class Children1(override val name : String, override val age : Int) : PersonInterfaces {
+    override fun getDetail(): String {
+        return "the person's name is $name"
+    }
+}
+class InheritanceChildren1( name : String, age : Int) : PersonInheritance(name, age) {
+}
+class Children2(override val name : String, override val age : Int) : PersonInterfaces {
+    override fun getDetail(): String {
+        return "the person's name is $name"
+    }
+}
+class InheritanceChildren2(name : String, age : Int) : PersonInheritance(name, age) {
 }
 
-class Children2(override val name : String, override val age : Int) : Interfaces, Inheritance(name, age) {
-}
-
-open class Inheritance(open val name : String, open val age : Int) {
-    fun inheritance(){
-
+open class PersonInheritance( val name : String,  val age : Int) {
+    open fun getDetail(){
         println("the name is $name and the age is $age")
     }
 }
 
-interface Interfaces {
+interface PersonInterfaces {
     val name : String
-
-    fun interfaces(): String {
-        return "the person's name is $name"
-    }
+    val age : Int
+    fun getDetail(): String
 }
